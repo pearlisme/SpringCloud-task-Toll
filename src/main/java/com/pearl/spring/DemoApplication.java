0c3d08605d4cca97e5d76e6ec98e702a4c410d8d
pe@rl <Muthubharathi.r@gmail.com>
2018-09-03 21:11:14 +0530

    added release code commit

--- src/main/java/com/pearl/spring/DemoApplication.java
+++ src/main/java/com/pearl/spring/DemoApplication.java
@@ -10,34 +10,36 @@ import org.springframework.context.annotation.Bean;
 @EnableTask
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 
-	@Bean
-	public TollProcessingTask tollProcessingTask(){
-		return  new TollProcessingTask();
-	}
+    @Bean
+    public TollProcessingTask tollProcessingTask() {
+        return new TollProcessingTask();
+    }
 
-	public class TollProcessingTask implements CommandLineRunner{
+    public class TollProcessingTask implements CommandLineRunner {
 
-		@Override
-		public void run(String... args) throws Exception {
-			if(null != args){
-				System.out.println("Parameter length is "+ args.length);
+        @Override
+        public void run(String... args) throws Exception {
+            if (null != args) {
+                System.out.println("Parameter length is " + args.length);
+
+                if (args.length != 0) {
+
+                    String stationId = args[0];
+                    String licensePlate = args[1];
+                    String timestamp = args[2];
+                    System.out.println("Station ID is " + stationId + ", Plate is " +
+                            licensePlate + ", timestamp is " + timestamp);
+                }
 
 
-				String stationId = args[0];
-				String licensePlate = args[1];
-				String timestamp = args[2];
-
-
-				System.out.println("Station ID is " + stationId + ", Plate is "+
-						licensePlate + ", timestamp is "+ timestamp);
-			}
-		}
-	}
+            }
+        }
+    }
 
 
 }
